0548b49e2ac2233d5a58cff86ce4c1c52c548945
Kasai Koji <streamwest1629@gmail.com>
2022-04-23 14:07:32 +0000

    #7: B問題解いた

--- atcoder/abc/114/B/template/src/main.rs
+++ atcoder/abc/114/B/template/src/main.rs
@@ -6,12 +6,25 @@ use std::collections::BinaryHeap as b_heap;
 use std::collections::VecDeque as deque;
 
 fn main() {
+    let comp: i32 = 753;
+    let mut nearest: i32 = 0;
+    let mut nearest_diff: i32 = comp;
+
     input! {
-        a: i32,
-        b: i32,
-        c: i32,
-        s: String,
+        st: String,
     }
 
-    println!("{} {}", a + b + c, s)
+    let s: &str = &st;
+
+    let v = s.chars();
+    for i in 0..(s.len() - 2) {
+        let check: i32 = s[i..i + 3].parse().unwrap();
+        let diff = (check - comp).abs();
+        if nearest_diff > diff {
+            nearest = check;
+            nearest_diff = diff;
+        }
+    }
+
+    println!("{}", nearest_diff)
 }
