83377100659bb0246445312deaef066368112bd7
Kasai Koji <streamwest1629@gmail.com>
2022-04-23 14:59:06 +0000

    #7: C問題解いた

--- atcoder/abc/114/C/template/src/main.rs
+++ atcoder/abc/114/C/template/src/main.rs
@@ -5,13 +5,33 @@ use std::collections::BTreeSet as set;
 use std::collections::BinaryHeap as b_heap;
 use std::collections::VecDeque as deque;
 
+fn resolve(add: i64, digit: i64, bs: i64, limit: i64) -> i64 {
+    let mut count = 0;
+    if add + digit * 3 <= limit {
+        count += resolve(add + digit * 3, digit * 10, bs | 1, limit);
+        if (bs | 1) == 7 {
+            count += 1
+        }
+    }
+    if add + digit * 5 <= limit {
+        count += resolve(add + digit * 5, digit * 10, bs | 2, limit);
+        if (bs | 2) == 7 {
+            count += 1
+        }
+    }
+    if add + digit * 7 <= limit {
+        count += resolve(add + digit * 7, digit * 10, bs | 4, limit);
+        if (bs | 4) == 7 {
+            count += 1
+        }
+    }
+    return count;
+}
+
 fn main() {
     input! {
-        a: i32,
-        b: i32,
-        c: i32,
-        s: String,
+        n: i64,
     }
 
-    println!("{} {}", a + b + c, s)
+    println!("{}", resolve(0, 1, 0, n))
 }
