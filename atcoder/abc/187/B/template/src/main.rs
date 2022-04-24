962ebf7be56b6b74f54440856554e3156c6c8ca6
Kasai Koji <streamwest1629@gmail.com>
2022-04-24 00:58:19 +0000

    #9: B問題解いた

--- atcoder/abc/187/B/template/src/main.rs
+++ atcoder/abc/187/B/template/src/main.rs
@@ -7,11 +7,21 @@ use std::collections::VecDeque as deque;
 
 fn main() {
     input! {
-        a: i32,
-        b: i32,
-        c: i32,
-        s: String,
+        n: i32,
+        pos: [(i32, i32); n],
+    };
+
+    let mut ans = 0;
+
+    for i in 0..pos.len() {
+        for j in (i + 1)..pos.len() {
+            let (i_x, i_y) = pos[i];
+            let (j_x, j_y) = pos[j];
+            if (i_x - j_x).abs() >= (i_y - j_y).abs() {
+                ans += 1;
+            }
+        }
     }
 
-    println!("{} {}", a + b + c, s)
+    println!("{}", ans)
 }
