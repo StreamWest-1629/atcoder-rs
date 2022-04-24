48e7b8af7611c79570de72f54c6a1ade453d9cce
Kasai Koji <streamwest1629@gmail.com>
2022-04-24 00:04:15 +0000

    #7: D問題解けた

--- atcoder/abc/114/D/template/src/main.rs
+++ atcoder/abc/114/D/template/src/main.rs
@@ -10,23 +10,65 @@ fn main() {
         n: i32,
     }
 
-    let mut n_divs = 0;
+    let mut n_divs = vec![0; 105];
+    let mut ans = 0i32;
 
     for val in 2..(n + 1) {
         let mut buf = val;
-        let mut div = 2;
-        while buf > 1 {
+        for div in 2..105 {
             while buf % div == 0 {
                 buf /= div;
-                n_divs += 1;
+                n_divs[div as usize] += 1;
             }
-            div += 1;
+            if buf == 1 {
+                continue;
+            }
         }
     }
 
-    if n_divs < 75 {
-        println!("0");
-        return;
-    } else {
+    // A^74
+    for i in 0..(n_divs.len()) {
+        if n_divs[i] >= 74 {
+            ans += 1;
+        }
     }
+
+    // A^24 * B^2
+    for i in 0..(n_divs.len()) {
+        if n_divs[i] >= 24 {
+            for j in 0..(n_divs.len()) {
+                if n_divs[j] >= 2 && i != j {
+                    ans += 1;
+                }
+            }
+        }
+    }
+
+    // A^14 * B*4
+    for i in 0..(n_divs.len()) {
+        if n_divs[i] >= 14 {
+            for j in 0..(n_divs.len()) {
+                if n_divs[j] >= 4 && i != j {
+                    ans += 1;
+                }
+            }
+        }
+    }
+
+    // A^4 * B^4 * C^2
+    for i in 0..(n_divs.len()) {
+        if n_divs[i] >= 4 {
+            for j in (i + 1)..(n_divs.len()) {
+                if n_divs[j] >= 4 {
+                    for k in 0..(n_divs.len()) {
+                        if n_divs[k] >= 2 && k != j && k != i {
+                            ans += 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    println!("{}", ans)
 }
