d5591240fa3fa163f9d1248823b38c16e364625c
Kasai Koji <streamwest1629@gmail.com>
2022-04-27 05:42:55 +0000

    #9: D問題といた

--- atcoder/abc/187/D/template/src/main.rs
+++ atcoder/abc/187/D/template/src/main.rs
@@ -7,11 +7,26 @@ use std::collections::VecDeque as deque;
 
 fn main() {
     input! {
-        a: i32,
-        b: i32,
-        c: i32,
-        s: String,
+        n: i32,
+        ab: [(i64, i64); n],
     }
 
-    println!("{} {}", a + b + c, s)
+    let mut diff: Vec<i64> = ab.iter().map(|(a, b)| 2 * a + b).collect();
+    diff.sort_by(|l, r| r.cmp(l));
+    let sorted = diff;
+
+    let mut sum_a: i64 = 0;
+    for (a, _) in ab {
+        sum_a += a;
+    }
+
+    let mut ans = 0;
+    for larger in sorted {
+        ans += 1;
+        sum_a -= larger;
+        if sum_a < 0 {
+            println!("{}", ans);
+            return;
+        }
+    }
 }
