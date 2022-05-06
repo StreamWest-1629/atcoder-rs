26e3353f0a5395c52320ba232af90dce6cbc49da
Kasai Koji <streamwest1629@gmail.com>
2022-05-06 05:15:47 +0000

    #13: D問題といた

--- atcoder/abc/239/D/template/src/main.rs
+++ atcoder/abc/239/D/template/src/main.rs
@@ -6,12 +6,41 @@ use std::collections::BinaryHeap as b_heap;
 use std::collections::VecDeque as deque;
 
 fn main() {
+    let mut prime: [i32; 205] = [0; 205];
+    for i in 2..201 {
+        if prime[i] == 0 {
+            prime[i] = 1;
+            for j in 2..100 {
+                if i * j > 201 {
+                    break;
+                } else if prime[i * j] == 0 {
+                    prime[i * j] = -1;
+                }
+            }
+        }
+    }
+
     input! {
         a: i32,
         b: i32,
         c: i32,
-        s: String,
+        d: i32,
     }
 
-    println!("{} {}", a + b + c, s)
+    for i in a..=b {
+        let mut takahashi = true;
+        for j in c..=d {
+            if prime[(i + j) as usize] == 1 {
+                takahashi = false;
+            }
+        }
+
+        if !takahashi {
+            println!("Takahashi");
+            return;
+        }
+    }
+
+    println!("Aoki");
+    return;
 }
